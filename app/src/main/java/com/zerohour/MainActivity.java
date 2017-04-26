96392dd3aced19387945ba96538714ba104da57f
Shankar Pilli <shankar.pilli@sparity.com>
2017-04-26 18:39:00 +0530

    Added Project to Git

---
+++ app/src/main/java/com/zerohour/MainActivity.java
@@ -0,0 +1,13 @@
+package com.zerohour;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
