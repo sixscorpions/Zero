42dc6ab4f0821c4bf300aa6a40b9634032bf09e1
Shankar Pilli <shankar.pilli@sparity.com>
2017-04-26 19:02:13 +0530

    Added splash screen

---
+++ app/src/main/java/com/zerohour/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.zerohour;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class SplashActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+}
