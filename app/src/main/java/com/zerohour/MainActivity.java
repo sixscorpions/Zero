eee7f6bb21f55d810aa74ed35a878fb193a57df0
Shankar Pilli <shankar.pilli@sparity.com>
2017-05-04 14:57:07 +0530

    Added items in the Zero hour and navigation drawer

--- app/src/main/java/com/zerohour/MainActivity.java
+++ app/src/main/java/com/zerohour/MainActivity.java
@@ -1,13 +1,88 @@
 package com.zerohour;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
 
-public class MainActivity extends Activity {
+import com.zerohour.fragments.AlienCarFragment;
+import com.zerohour.fragments.InviteFragment;
+import com.zerohour.fragments.MaidStatusFragment;
+import com.zerohour.fragments.NoticeBoardFragment;
+
+
+public class MainActivity extends AppCompatActivity implements FragmentDrawer.FragmentDrawerListener {
+
+    private static String TAG = MainActivity.class.getSimpleName();
+
+    private Toolbar mToolbar;
+    private FragmentDrawer drawerFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+
+        drawerFragment = (FragmentDrawer)
+                getSupportFragmentManager().findFragmentById(R.id.fragment_navigation_drawer);
+        drawerFragment.setUp(R.id.fragment_navigation_drawer, (DrawerLayout) findViewById(R.id.drawer_layout), mToolbar);
+        drawerFragment.setDrawerListener(this);
+
+        // display the first navigation drawer view on app launch
+        displayView(0);
     }
+
+    private void displayView(int position) {
+        Fragment fragment = null;
+        String title = getString(R.string.app_name);
+        switch (position) {
+            case 0:
+                fragment = new InviteFragment();
+                title = getString(R.string.nav_item_invite);
+                break;
+            case 1:
+                fragment = new MaidStatusFragment();
+                title = getString(R.string.nav_item_maid_status);
+                break;
+            case 2:
+                fragment = new AlienCarFragment();
+                title = getString(R.string.nav_item_alien_car);
+                break;
+            case 3:
+                fragment = new NoticeBoardFragment();
+                title = getString(R.string.nav_item_notice_board);
+                break;
+            case 5:
+                fragment = new NoticeBoardFragment();
+                title = getString(R.string.nav_item_complaints);
+                break;
+            default:
+                break;
+        }
+
+        if (fragment != null) {
+            FragmentManager fragmentManager = getSupportFragmentManager();
+            FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+            fragmentTransaction.replace(R.id.container_body, fragment);
+            fragmentTransaction.commit();
+
+            // set the toolbar title
+            getSupportActionBar().setTitle(title);
+        }
+    }
+
+    @Override
+    public void onDrawerItemSelected(View view, int position) {
+        displayView(position);
+    }
+
 }
---
+++ app/src/main/java/com/zerohour/fragments/NoticeBoardFragment.java
@@ -0,0 +1,31 @@
+package com.zerohour.fragments;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.zerohour.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class NoticeBoardFragment extends Fragment {
+
+
+    public NoticeBoardFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notice_board, container, false);
+    }
+
+}
