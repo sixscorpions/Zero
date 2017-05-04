1f3ed1d55805c0b2c4215027062ba46cd666bf4f
Shankar Pilli <shankar.pilli@sparity.com>
2017-05-04 18:29:45 +0530

    Added validations for Party invite fragment

--- app/src/main/java/com/zerohour/fragments/PartyInviteFragment.java
+++ app/src/main/java/com/zerohour/fragments/PartyInviteFragment.java
@@ -2,20 +2,23 @@ package com.zerohour.fragments;
 
 
 import android.os.Bundle;
-import android.support.design.widget.TextInputLayout;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.zerohour.MainActivity;
 import com.zerohour.R;
 import com.zerohour.utils.Utility;
 
+import java.util.ArrayList;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -37,16 +40,24 @@ public class PartyInviteFragment extends Fragment {
     TextView tv_time;
 
 
+    @BindView(R.id.ll_first_contact)
+    LinearLayout ll_first_contact;
     @BindView(R.id.tv_first_contact)
     TextView tv_first_contact;
     @BindView(R.id.tv_first_contact_remove)
     TextView tv_first_contact_remove;
 
+    @BindView(R.id.ll_second_contact)
+    LinearLayout ll_second_contact;
     @BindView(R.id.tv_second_contact)
     TextView tv_second_contact;
     @BindView(R.id.tv_first_second_remove)
     TextView tv_first_second_remove;
 
+    @BindView(R.id.tv_more)
+    TextView tv_more;
+
+    private ArrayList<String> numbersSelected;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -63,13 +74,51 @@ public class PartyInviteFragment extends Fragment {
     }
 
     private void initUI() {
+        numbersSelected = new ArrayList<>();
         tvContactsImage.setTypeface(Utility.getMaterialIconsRegular(mParent));
         tv_add_image.setTypeface(Utility.getMaterialIconsRegular(mParent));
         tv_date.setTypeface(Utility.getMaterialIconsRegular(mParent));
         tv_time.setTypeface(Utility.getMaterialIconsRegular(mParent));
         tv_first_contact_remove.setTypeface(Utility.getMaterialIconsRegular(mParent));
         tv_first_second_remove.setTypeface(Utility.getMaterialIconsRegular(mParent));
+        tv_more.setTypeface(Utility.getMaterialIconsRegular(mParent));
     }
 
+    @OnClick(R.id.tv_add_image)
+    void addNumber() {
+        if (isValidFields()) {
+            numbersSelected.add(etMobileNumber.getText().toString());
+            if (numbersSelected != null && numbersSelected.size() == 1) {
+                etMobileNumber.setText("");
+                ll_first_contact.setVisibility(View.VISIBLE);
+                tv_first_contact.setText("" + numbersSelected.get(0));
+            } else if (numbersSelected != null && numbersSelected.size() == 2) {
+                etMobileNumber.setText("");
+                ll_first_contact.setVisibility(View.VISIBLE);
+                tv_first_contact.setText("" + numbersSelected.get(0));
+                ll_second_contact.setVisibility(View.VISIBLE);
+                tv_second_contact.setText("" + numbersSelected.get(1));
+                tv_more.setVisibility(View.GONE);
+            } else {
+                etMobileNumber.setText("");
+                ll_first_contact.setVisibility(View.VISIBLE);
+                tv_first_contact.setText("" + numbersSelected.get(0));
+                ll_second_contact.setVisibility(View.VISIBLE);
+                tv_second_contact.setText("" + numbersSelected.get(1));
+                tv_more.setVisibility(View.VISIBLE);
+            }
+        }
+    }
 
+    private boolean isValidFields() {
+        boolean isValid = true;
+        if (Utility.isValueNullOrEmpty(etMobileNumber.getText().toString())) {
+            Utility.showToastMessage(mParent, "Please enter mobile number");
+            isValid = false;
+        } else if (etMobileNumber.getText().toString().length() < 10) {
+            Utility.showToastMessage(mParent, "Please enter valid mobile number");
+            isValid = false;
+        }
+        return isValid;
+    }
 }
--- app/src/main/java/com/zerohour/utils/Utility.java
+++ app/src/main/java/com/zerohour/utils/Utility.java
@@ -2,6 +2,7 @@ package com.zerohour.utils;
 
 import android.content.Context;
 import android.graphics.Typeface;
+import android.widget.Toast;
 
 /**
  * Created by Shankar on 5/4/2017.
@@ -30,6 +31,40 @@ public class Utility {
      **/
     public static Typeface getMaterialIconsRegular(Context context) {
         return Typeface.createFromAsset(context.getAssets(), "fonts/matireal_icons_regular.ttf");
+    }
+
+    /**
+     * Check the value is null or empty
+     *
+     * @param value Value of that string
+     * @return Boolean returns the value true or false
+     */
+    public static boolean isValueNullOrEmpty(String value) {
+        boolean isValue = false;
+        if (value == null || value.equals("") || value.equals("0.0")
+                || value.equals("null") || value.trim().length() == 0) {
+            isValue = true;
+        }
+        return isValue;
+    }
+
+    /**
+     * Shows toast message
+     *
+     * @param context Context of the class
+     * @param message What message you have to show
+     */
+    public static void showToastMessage(Context context, String message) {
+        try {
+            if (!isValueNullOrEmpty(message) && context != null) {
+                final Toast toast = Toast.makeText(
+                        context.getApplicationContext(), message,
+                        Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
 }
